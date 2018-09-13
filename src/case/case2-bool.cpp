e400efdbfa8ab75adb02a6a88b52458c286fadf4
czb <834832459@qq.com>
2018-09-14 00:11:19 +0800

    1.增加了学习文档。 2.学习了快键键 3. 工具

---
+++ src/case/case2-bool.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    //nihao asdfasdf
+            //sdfasdfas
+    int a = 1; // asdfasdfddad
+    if (true)
+    {
+        cout << a << endl;
+    }
+    int b = 4;
+    int adgfa = 4;
+    char* c = NULL;
+
+    system("pause");
+    return 1;
+}
